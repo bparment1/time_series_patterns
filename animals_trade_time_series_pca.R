4bb94ac4a69414a5c36224cd284039285f77dbe5
bparment1 <benoit.parmentier@gmail.com>
2017-05-23 14:30:00 -0400

    adding function to run pca analysis

--- animals_trade_time_series_pca.R
+++ animals_trade_time_series_pca.R
@@ -2,7 +2,7 @@
 ##
 ## 
 ## DATE CREATED: 05/22/2017
-## DATE MODIFIED: 05/22/2017
+## DATE MODIFIED: 05/23/2017
 ## AUTHORS: Benoit Parmentier and Elizabeth Daut
 ## Version: 2
 ## PROJECT: Animals trade
@@ -27,13 +27,15 @@ library(nlme) #mixed models and GLS
 library(forecast) #moving average, ARIMA and other tools
 library(psych) #pca/eigenvector decomposition functionalities
 library(GPArotation) #rotation functions for PCA and EFA
-library(plotrix)
+library(plotrix) #additional plotting and drawing options 
 
 ###### Functions used in this script
 
 script_path <- "/research-home/bparmentier/Data/projects/animals_trade/scripts"
-function_time_series_analyses <- "animals_trade_time_series_analyses_functions_05222017d.R" #PARAM 1
+function_time_series_analyses <- "animals_trade_time_series_analyses_functions_05222017e.R" #PARAM 1
+functions_pca_script <- "pca_eof_functions_05232017c.R" #PARAM 1
 source(file.path(script_path,function_time_series_analyses ))
+source(file.path(script_path,functions_pca_script)) #source all functions used in this script 1.
 
 #####  Parameters and argument set up ###########
 
@@ -100,121 +102,36 @@ names_col <- as.character(df_dat_animals$country)
 ###########################
 ### PART II: PCA analyses #######
 
-if(is.null(n_pca)){
-  npc <- ncol(df_dat)
-}
+## Use revised functions that combines everything:
 
-##### Performing PCA
-  
+selected_var1 <- names(df_dat)
+npc <- length(selected_var1)
+
+data_df_subset <- subset(df_dat,select=selected_var1)
+
+names(data_df_subset)
+
 ## By default we use the correlation matrix in T mode, this will be changed later on.
 #debug(run_pca_fun)
-principal_pca_obj <- run_pca_fun(A=df_dat,mode="T",rotation_opt="none",matrix_val=NULL,npc=npc,loadings=TRUE,scores_opt=TRUE)
-pca_mod <- principal_pca_obj$pca_principal
+out_suffix_str <- paste0("pca_analyses1_cor_",out_suffix)
+npc <- length(selected_var1)
+var_labels <- selected_var1
+pcs_selected <- list(c(1,2),c(2,3),c(3,4),c(4,5))
+#undebug(run_pca_analysis)  
+run_pca_analysis(data_df=data_df_subset,
+                 matrix_val=NULL,
+                 npc=npc,
+                 pcs_selected=pcs_selected,
+                 time_series_loadings=F,
+                 save_opt=FALSE,
+                 var_labels=var_labels,
+                 mode_val=T, 
+                 rotation_opt="none",
+                 scores_opt=produce_scores,
+                 out_dir=".",
+                 out_suffix=out_suffix_str)
 
-#Save eigenvalues
-df_eigen <- data.frame(variable=names(df_dat),eigenvalue=pca_mod$values)
-df_eigen_filename <- file.path(out_dir,paste("df_eigen_",out_suffix_str,".txt",sep=""))
-write.table(df_eigen,file=df_eigen_filename,sep=",")
-
-#Save correlation matrix
-out_suffix_str <- paste0("cor_",out_suffix)
-pca_input_square_matrix_filename <- file.path(out_dir,paste("pca_input_square_matrix_",out_suffix_str,".txt",sep=""))
-write.table(principal_pca_obj$matrix_val,file=pca_input_square_matrix_filename,sep=",")
-
-#Save loadings matrix
-pca_loadings_filename <- file.path(out_dir,paste("pca_loadings_",out_suffix,".txt",sep=""))
-write.table(principal_pca_obj$loadings,file=pca_loadings_filename,sep=",")
-
-#Save scores if produce_scores==T
-if(produce_scores==T){
-  #
-  pca_scores_filename <- file.path(out_dir,paste("pca_scores_",out_suffix,".txt",sep=""))
-  write.table(principal_pca_obj$scores,file=pca_scores_filename,sep=",")
-}
-
-############################
-##### plotting of PCAs
-
-#Add option for non time series
-plot_pca_loadings_time_series(1,pca_mod=pca_mod,dates_val,out_dir=out_dir, out_suffix=out_suffix)
-
-lapply(1:6,FUN=plot_pca_loadings_time_series,pca_mod=pca_mod,dates_val,out_dir=out_dir,out_suffix=out_suffix)
-
-## Add scree plot of eigen values
-png_filename <- file.path(out_dir,paste("Figure_scree_plots_eigen_values_components_",out_suffix,".png",sep=""))
-
-res_pix<-960
-col_mfrow<- 1
-row_mfrow<- 1
-png(filename= png_filename,
-    width=col_mfrow*res_pix,height=row_mfrow*res_pix)
-
-plot(pca_mod$values,
-     main="Scree plot: Eigen values",
-     type="b",
-     ylab="Eigenvalue",
-     xlab="components")
-
-dev.off()
-
-## Add scree plot of variance from components
-png_filename <- file.path(out_dir,paste("Figure_scree_plots_percent_variance_components_",out_suffix,".png",sep=""))
-
-res_pix<-960
-col_mfrow<- 1
-row_mfrow<- 1
-png(filename= png_filename,
-    width=col_mfrow*res_pix,height=row_mfrow*res_pix)
-
-plot(pca_mod$values/sum(pca_mod$values)*100,
-     main="Scree plot: Variance from component in %",
-     type="b",
-     ylab="% variance",
-     xlab="components")
-
-dev.off()
-
-######## Plot in pc space
-
-loadings_df <- as.data.frame(pca_mod$loadings[,pcs_selected])
-#pca_loadings_dz <- zoo(loadings_df,dates_val) #create zoo object from data.frame and date sequence object
-
-pcs_selected <- c(1,2)
-
-
-png_filename <- file.path(out_dir,paste("Figure_pc_components_space_loadings",pcs_selected[1],pcs_selected[2],"_",out_suffix,".png",sep=""))
-
-res_pix<-960
-col_mfrow<- 1
-row_mfrow<- 1
-png(filename= png_filename,
-    width=col_mfrow*res_pix,height=row_mfrow*res_pix)
-#par(mfrow=c(1,2))
-
-plot(loadings_df[,1],loadings_df[,2],
-     type="p",
-     pch = 20,
-     col ="blue",
-     xlab=names(loadings_df)[1],
-     ylab=names(loadings_df)[2],
-     ylim=c(-1,1),
-     xlim=c(-1,1),
-     axes = FALSE,
-     cex.lab = 1.2)
-axis(1, at=c(-1,-0.8,-0.6,-0.4,-0.2,0,0.2,0.4,0.6,0.8,1),cex=1.2) # "1' for side=below, the axis is drawned  on the right at location 0 and 1
-axis(2, las=1,at=c(-1,-0.8,-0.6,-0.4,-0.2,0,0.2,0.4,0.6,0.8,1),cex=1.2) # "1' for side=below, the axis is drawned  on the right at location 0 and 1
-
-box()    #This draws a box...
-
-title(paste0("Loadings for component ", names(loadings_df)[1]," and " ,names(loadings_df)[2] ))
-draw.circle(0,0,c(1.0,1),nv=200)#,border="purple",
-text(loadings_df[,1],loadings_df[,2],rownames(loadings_df),pos=1,cex=1)            
-grid(2,2)
-
-dev.off()
-
-
-
-
+#functions_pca_script <- "pca_eof_functions_05232017c.R" #PARAM 1
+#source(file.path(script_path,functions_pca_script)) #source all functions used in this script 1.
 
 ################### End of script ################
