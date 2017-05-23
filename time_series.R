252ca6c57eb03e4be4b9f4fae9a379cb817148d6
bparment1 <benoit.parmentier@gmail.com>
2017-05-23 14:30:00 -0400

    example to teach how to make a function for Theil Sen slope estimator

--- time_series.R
+++ time_series.R
@@ -2,25 +2,25 @@
 ##
 ## 
 ## DATE CREATED: 05/15/2017
-## DATE MODIFIED: 05/16/2017
+## DATE MODIFIED: 05/23/2017
 ## AUTHORS: Benoit Parmentier and Elizabeth Daut
 ## Version: 1
 ## PROJECT: Animals trade
 ## ISSUE: 
 ## TO DO:
 ##
-## COMMIT: quick example of time series example
+## COMMIT: making a function for Theil Sen slope estimator
 ##
 
 ###################################################
 #
 
-###### Functions used in this script
+###### Library used
 
 library(xts) #extension for time series object and analyses
 library(zoo) # time series object and analysis
 library(mblm) #Theil Sen estimator
-library(lubridate)
+library(lubridate) 
 
 ###### Functions used in this script
 
@@ -75,11 +75,12 @@ df_w_ts <- window(df_ts,start=as.Date("2016-01-01"),end= as.Date("2017-01-01"))
 plot(df_w_ts[,1:3])
 
 df_agg_ts <- aggregate(df_ts, by=as.yearqtr)
+df_agg_ts <- aggregate(df_ts, by=year)
 class(df_agg_ts)
-
 plot(df_agg_ts[,1:3])
+plot(df_agg_ts[,1:3],plot.type="single")
 
-
+#### Compare quarterly aggregated to original monthly data
 par(mfrow=c(2,1))
 plot(df_ts[,1])
 plot(df_agg_ts[,1],col="red")
@@ -95,30 +96,55 @@ plot((df_ts[,1]))
 
 ## Fourier Analysis, Wavelet etc.
 
-test<-(fft(df_ts[,1]))
-plot(test)
-View(test)
-class(fft(df_ts[,1]))
+#test<-(fft(df_ts[,1]))
+#plot(test)
+#View(test)
+#class(fft(df_ts[,1]))
 
-plot(Re(fft(test))^2)
+#plot(Re(fft(test))^2)
 
 ### Theil Sen slope slope calculation
 
 #make this a function later: example with the first country
-
-time_index <- 1:nrow(df_ts)
-subset_name <- names(df_ts)[1]
-df_mblm <- subset(df_ts,select=subset_name) #subset using relevant name
-
-df_mblm <- as.data.frame(df_mblm ) #convert to data.frame since it was a zoo df
-df_mblm$time_index <- time_index
-names(df_mblm)
-formula_str <- paste0(names(df_mblm)[1]," ~ ","time_index")
-formula_mblm <- as.formula(formula_str)
-
-mod_mblm<- mblm(formula_mblm,df_mblm)
-#can get the conf interval or significance if wanted
-plot(mod_mblm)
+mod_mblm_test <- calculate_theil_sen_time_series(i=1,data_df=df_ts,out_dir=".",out_suffix="time_series_analyses_05232017")
+  
+calculate_theil_sen_time_series <- function(i,data_df,out_dir,out_suffix){
+  #This function generates Theil Sen slope estimate using mblm package.
+  #
+  #INPUTS
+  #1) i: column index for input data.frame, used as y variable (trend variable)
+  #2) data_df: input data containing time series
+  #4) out_dir: output directory
+  #5) out_suffix: output suffix appended to written output on drive
+  #
+  #OUTPUTS
+  #1)
+  #
+  #TO DO: 
+  
+  ############# BEGIN FUNCTION #############
+  
+  #setting up the data input: data.frame with 2 columns
+  time_index <- 1:nrow(data_df) #x
+  subset_name <- names(data_df)[i]
+  df_mblm <- subset(df_ts,select=subset_name) #subset using relevant name
+  
+  df_mblm <- as.data.frame(df_mblm) #convert to data.frame since it was a zoo df
+  df_mblm$time_index <- time_index
+  #names(df_mblm)
+  
+  ### automate formula input
+  formula_str <- paste0(names(df_mblm)[i]," ~ ","time_index")
+  formula_mblm <- as.formula(formula_str) #transform object into formula
+  
+  mod_mblm<- mblm(formula_mblm,df_mblm)
+  #can get the conf interval or significance if wanted
+  #plot(mod_mblm)
+  
+  #### Prepare object to return
+  
+  return(mod_mblm)
+}
 
 
 ################### End of script ################
