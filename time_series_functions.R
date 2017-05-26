5dc4ea55a5913a56a4915e0b9def304a0108417c
bparment1 <benoit.parmentier@gmail.com>
2017-05-26 14:30:00 -0400

    modification of theil sen function to store slope and other info in data.frame

--- time_series_functions.R
+++ time_series_functions.R
@@ -2,7 +2,7 @@
 ##
 ## 
 ## DATE CREATED: 05/23/2017
-## DATE MODIFIED: 05/23/2017
+## DATE MODIFIED: 05/25/2017
 ## AUTHORS: Benoit Parmentier and Elizabeth Daut
 ## Version: 1
 ## PROJECT: Animals trade
@@ -45,7 +45,7 @@ load_obj <- function(f){
 }
 
 
-calculate_theil_sen_time_series <- function(i,data_df,out_dir,out_suffix){
+calculate_theil_sen_time_series <- function(i,data_df,save_opt=FALSE,out_dir=".",out_suffix=""){
   #This function generates Theil Sen slope estimate using mblm package.
   #
   #INPUTS
@@ -62,9 +62,16 @@ calculate_theil_sen_time_series <- function(i,data_df,out_dir,out_suffix){
   ############# BEGIN FUNCTION #############
   
   #setting up the data input: data.frame with 2 columns
-  time_index <- 1:nrow(data_df) #x
+  time_index <- 1:nrow(data_df) #
   subset_name <- names(data_df)[i]
   df_mblm <- subset(data_df,select=subset_name) #subset using relevant name
+  
+  #stores dates for later processing
+  if(inherits(data_df,"zoo")){
+    dates_val <- date(df_mblm)
+  }else{
+    dates_val <- NULL
+  }
   
   df_mblm <- as.data.frame(df_mblm) #convert to data.frame since it was a zoo df
   df_mblm$time_index <- time_index
@@ -75,18 +82,42 @@ calculate_theil_sen_time_series <- function(i,data_df,out_dir,out_suffix){
   formula_mblm <- as.formula(formula_str) #transform object into formula
   
   mod_mblm <- mblm(formula_mblm,df_mblm)
-  #can get the conf interval or significance if wanted
-  #plot(mod_mblm)
-  coef_theil_sen <- coef(mod_mblm)
+  
+  ##### Extract information from model object mblm
+
+  slope_theil_sen <- coef(mod_mblm)[2]
+  intercept_theil_sen <- coef(mod_mblm)[1]
+  ID_ts <- subset_name
+  method_str <- "theil_sen"
+
+  if(!is.null(dates_val)){
+    nt <- length(dates_val)
+    start_date <- dates_val[1]
+    end_date <- dates_val[nt]
+  }
+
+  duration_ts <- nt
+  slope_sign <- sign(slope_theil_sen)
+  
+  df_theil_sen <- data.frame(ID_ts=ID_ts,
+                             intercept=intercept_theil_sen,
+                             slope=slope_theil_sen,
+                             slope_sign=slope_sign,
+                             method=method_str,
+                             start_date= start_date,
+                             end_date = end_date,
+                             duration=nt)
   
   #### Prepare object to return
-  obj_theil_sen <- list(mod_mblm,coef_theil_sen)
-  names(obj_theil_sen) <- c("mod_mblm","coef_theil_sen")
+  obj_theil_sen <- list(mod_mblm,df_theil_sen)
+  names(obj_theil_sen) <- c("mod_mblm","df_theil_sen")
   
   ##### save to disk
   
-  obj_theil_sen_filename <- file.path(out_dir,paste("theil_sen_obj_",subset_name,"_",out_suffix,".RData",sep=""))
-  save(obj_theil_sen,file= obj_theil_sen_filename)
+  if(save_opt==TRUE){
+    obj_theil_sen_filename <- file.path(out_dir,paste("theil_sen_obj_",subset_name,"_",out_suffix,".RData",sep=""))
+    save(obj_theil_sen,file= obj_theil_sen_filename)
+  } 
   
   return(obj_theil_sen)
 }
