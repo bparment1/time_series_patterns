5db59e0ae07b8f85c560e40e44254c1b5742df44
bparment1 <benoit.parmentier@gmail.com>
2017-06-01 14:30:00 -0400

    moving function import to function script

--- processing_data_google_search_time_series.R
+++ processing_data_google_search_time_series.R
@@ -1,38 +1,123 @@
+############### SESYNC Research Support: Animals Trade ########## 
+## Processing data from google search on species for the animals-trade project at SESYNC.
+## 
+## DATE CREATED: 05/31/2017
+## DATE MODIFIED: 06/01/2017
+## AUTHORS: Benoit Parmentier and Elizabeth Daut 
+## Version: 1
+## PROJECT: Animals trade by Elizabeth Daut
+## ISSUE: 
+## TO DO:
+##
+## COMMIT: documenting and testing function to import data sent by google
+##
+## Links to investigate:
 
+###################################################
+#
 
-in_dir <- "/nfs/edaut-data/Time Series MARSS"
-infile_name <- "gst40k_original_5262017.csv"
-out_dir <- "/nfs/edaut-data/Time Series MARSS/outputs"
-out_suffix <- "imported_ts_05312017"
+###### Library used
+###Loading R library and packages                                                      
+
+library(raster)                 # loading the raster package
+library(gtools)                 # loading R helper programming tools/functions
+library(sp)                     # spatial objects in R
+library(gplots)                 # plotting functions such as plotCI
+library(rgdal)                  # gdal driver for R
+library(RColorBrewer)           # color scheme, palettes used for plotting
+library(gdata)                  # read different format (including .xlsx)
+library(plotrix)                # plot options and functions 
+library(rasterVis)              # raster visualization
+library(colorRamps)             # contains matlab.like palette
+library(zoo)                    # time series objects and methods
+library(xts)                    # extension of time series objects
+library(BMS)                    # contains hex2bin and bin2hex
+library(bitops)                 # bit operations
+library(gtools)                 #
+library(maptools)               #
+library(rgeos)                  # spatial analysis, topological and geometric operations e.g. interesect, union, contain etc.
+library(sphet)                  # spatial analyis, regression eg.contains spreg for gmm estimation
+library(forecast)               # arima and other time series methods
+library(lubridate)              # date and time handling tools
+library(parallel)               # access to parallelization functions
+require(RCurl)
+require(stringr)
+require(XML)
+
+###### Functions used in this script sourced from other files
+
+#function_rainfall_time_series_NEST_analyses <- "rainfall_time_series_NEST_function_12112015.R" #PARAM 1
+#script_path <- "/home/bparmentier/Google Drive/NEST/R_NEST" #path to script #PARAM 
+#script_path <- "/home/parmentier/Data/rainfall/NEST"
+#source(file.path(script_path,function_rainfall_time_series_NEST_analyses)) #source all functions used in this script 1.
+
+##### Functions used in this script 
+
+create_dir_fun <- function(outDir,out_suffix=NULL){
+  #if out_suffix is not null then append out_suffix string
+  if(!is.null(out_suffix)){
+    out_name <- paste("output_",out_suffix,sep="")
+    outDir <- file.path(outDir,out_name)
+  }
+  #create if does not exists
+  if(!file.exists(outDir)){
+    dir.create(outDir)
+  }
+  return(outDir)
+}
+
+#Used to load RData object saved within the functions produced.
+load_obj <- function(f){
+  env <- new.env()
+  nm <- load(f, env)[1]
+  env[[nm]]
+}
+
+## Add processing of downloaded files!!
+## 1) crop and reproject if needed
+## 2) creation of TimeRaster object with summaries?
+## 3)
+### Other functions ####
+
+function_processing_data <- "processing_data_google_search_time_series_functions_06012017.R" #PARAM 1
+script_path <- "/nfs/bparmentier-data/Data/projects/animals_trade/scripts" #path to script #PARAM 
+source(file.path(script_path,function_processing_data)) #source all functions used in this script 1.
+
+############################################################################
+#####  Parameters and argument set up ###########
+
+in_dir <- "/nfs/bparmentier-data/Data/projects/animals_trade/data" #local bpy50 , param 1
+#in_dir <- "/home/parmentier/Data/rainfall/NEST" #NCEAS, param 
+out_dir <- "/nfs/bparmentier-data/Data/projects/animals_trade/outputs" #param 2
+
+#Elizabeth inputs
+#in_dir <- "/nfs/edaut-data/Time Series MARSS"
+#out_dir <- "/nfs/edaut-data/Time Series MARSS/outputs"
 
 scaling <- 1000
-start_date="2004-01-01"
+infile_name <- "gst40k_original_5262017.csv"
 
-import_data_ts <- function(infile_name,in_dir=".",scaling=1,n_col_start_date=4,start_date="2004-01-01",end_date=NULL,out_dir=".", out_suffix=""){
-  #Import data sent by google and format columns for future processing and visualization.
-  
-  ##### BEGIN FUNCTION ####
-  
-  ### Add quote="" otherwise EOF warning and error in reading
-  df <- read.table(file.path(in_dir,infile_name),sep=",",fill=T,quote="",header=F)
-  
-  #remove first row that contains names
-  #rename columns using:
-  #n_col_start_date <- 3 #may need to be changed
-  n_col <- ncol(df)
-  nt <- n_col - n_col_start_date #we are dropping the last date because it is often incomplete
-  
-  range_dates <- seq.Date(from=as.Date(start_date),by="month",length.out = nt )
-  range_dates_str <- as.character(range_dates)
-  #class(range_dates)
-  #start_date
-  names_col <- c("g_id","sci_name","country",range_dates_str)
-  df <- df[-1,-n_col] #remove the first row with incomplete header and last column with incomplete data
-  names(df) <- names_col
-  df[,n_col_start_date] <- sub('"',"",df[,n_col_start_date])
-  
-  out_filename_tmp <- sub(extension(infile_name),"",infile_name)#
-  out_filename <- file.path(out_dir,paste0(out_filename_tmp,"_",out_suffix,".csv"))
-  write.table(df,filename=out_filename)
-  return(out_filename)
-}
+start_date <- "2004-01-01"
+end_date <- NULL # param 4
+
+num_cores <- 4 #param 8
+create_out_dir_param=TRUE # param 9
+
+NA_value <- -9999 # param 10
+NA_flag_val <- NA_value #param 11
+
+out_suffix <-"processing_ts_06012017" #output suffix for the files and ouptu folder #param 12
+
+#download_file <- FALSE #param 14
+#unzip_files <- F #param 15
+
+######### PART 0: Set up the output dir ################
+
+
+lf_processed <- import_data_ts(infile_name,in_dir=".",
+                               scaling=1,
+                               n_col_start_date=4,
+                               start_date="2004-01-01",
+                               end_date=NULL,
+                               out_dir=".", 
+                               out_suffix="")
