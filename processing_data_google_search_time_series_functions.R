a7c9101ce6855b850c00e2acc16880546b635175
bparment1 <benoit.parmentier@gmail.com>
2017-12-11 14:30:00 -0500

    fixing errors in import function google searches version 2

--- processing_data_google_search_time_series_functions.R
+++ processing_data_google_search_time_series_functions.R
@@ -2,7 +2,7 @@
 ## Functions used in the processing of data from google search on species for the animals-trade project at SESYNC.
 ## 
 ## DATE CREATED: 05/31/2017
-## DATE MODIFIED: 12/03/2017
+## DATE MODIFIED: 12/11/2017
 ## AUTHORS: Benoit Parmentier and Elizabeth Daut 
 ## Version: 1
 ## PROJECT: Animals trade by Elizabeth Daut
@@ -36,7 +36,20 @@ library(ggplot2)
 ###### Functions used in this script
 
 import_data_ts <- function(infile_name,version=1,in_dir=".",scaling=1,n_col_start_date=4,start_date="2011-01-01",end_date=NULL,out_dir=".", out_suffix=""){
-  #Import data sent by google and format columns for future processing and visualization.
+  #
+  #Goal: Import data sent by google and format columns for future processing and visualization.
+  #
+  #INPUTS:
+  #1) infile_name: text file containing data in csv format
+  #2) version: indicate which version, version 1 contains gid while version does not
+  #3) in_dir: input directory
+  #4) n_col_start_date: first column containing date information
+  #5) start_date: starte date for the dataset, default is "2011-01-01"
+  #6) end_date: end data for the dataset, default is NULL
+  #7) out_dir: output directory, default is current dir "."
+  #8) out_suffix: suffix string to add to outputs
+  #OUTPUTS
+  #1) out_filename: output filename
   
   ##### BEGIN FUNCTION ####
   
@@ -46,7 +59,7 @@ import_data_ts <- function(infile_name,version=1,in_dir=".",scaling=1,n_col_star
   #remove first row that contains names
   #rename columns using:
   n_col <- ncol(df)
-  nt <- n_col - n_col_start_date #number of time steps
+  nt <- n_col - n_col_start_date + 1 #number of time steps
   
   range_dates <- seq.Date(from=as.Date(start_date),by="month",
                           length.out = nt )
@@ -56,17 +69,23 @@ import_data_ts <- function(infile_name,version=1,in_dir=".",scaling=1,n_col_star
     #If version1, we are dropping the last date because it is often incomplete
     names_col <- c("g_id","sci_name","country",range_dates_str)
     df <- df[-1,-n_col] #remove the first row with incomplete header and last column with incomplete data
+    names(df) <- names_col
+    df[,n_col_start_date] <- sub('"',"",df[,n_col_start_date])
   }
   
   if(version==2){
+    
     #if version2, we do not drop the last column and the data does not contain gid
     names_col <- c("sci_name","country",range_dates_str)
     df <- df[-1,] #remove the first row with incomplete header, keep the last column
+    names(df) <- names_col
+    df[,n_col_start_date] <- sub('"',"",df[,n_col_start_date])
+    df[,n_col] <- sub('"',"",df[,n_col])
+    
+    #df[,62]
+    #[997] "0.2015965068465905\""   "0.21044053064948223\""  "0\""                    "0.73523085537128918\"" 
   }
-  
-  names(df) <- names_col
-  df[,n_col_start_date] <- sub('"',"",df[,n_col_start_date])
-  
+
   ### Apply scaling factor
   
   out_filename_tmp <- sub(extension(infile_name),"",infile_name)#
