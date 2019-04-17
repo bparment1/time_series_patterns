df5718f4a548c50b5d094873aaa5a45ad210f4e3
Benoit Parmentier <bparmentier@sshgw02.research.sesync.org>
2019-04-17 14:30:00 -0400

    extract amplitudes, phases from coefficients of regression

--- harmonic_regression.R
+++ harmonic_regression.R
@@ -107,10 +107,11 @@ harmonic_regression<- function(y,n,harmonic_val=NULL,mod_obj=F){
   
   #n<-24
   
-  omega_val = lapply(p,function(p){2*pi*p/n})
+  #omega_val = lapply(p,function(p){2*pi*p/n})
   
-  fit_harmonic(omega,n,y){
+  fit_harmonic(p,n,y,mod_obj=F){
     t <- 1:n
+    omega=2*pi*p/n
     cos_val =cos(omega*t)
     sin_val =sin(omega*t)
     
@@ -118,11 +119,29 @@ harmonic_regression<- function(y,n,harmonic_val=NULL,mod_obj=F){
     #plot(sin_val)
     
     in_df <- data.frame(y=y,cos_val=cos_val,sin_val=sin_val)
-    mod <- lm(y~cos_val + sin_val,data=in_df)
+    mod <- lm(y~ sin_val+ cos_val ,data=in_df)
     summary(mod)
-    mod$coefficients
+    a <- mod$coefficients[2] #sine term
+    b <- mod$coefficients[3] #cosine term
+    A0 <- mod$coefficients[3] #mean
     
-    return()
+    A = sqrt(a^2 + b^2)
+    phase = atan(-b/a)
+    
+    ## Add p values later?
+    
+    harmonic_df <- data.frame(A0=A0,A=A,a=a,b=b,phase=phase,harmonic=p,omega=omega)
+    
+    ###
+    harmonic_obj <- harmonic_df
+    
+    if(mod_obj==T){
+      harmonic_obj <- list(harmonic_df=harmonic_df,mod=mod)
+    }else{
+      harmonic_obj <- list(harmonic_df=harmonic_df)
+    }
+    ### 
+    return(harmonic_obj)
   }
   
   plot(y)
