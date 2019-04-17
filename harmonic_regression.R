d1520657466e0a25a64dc61b2abb4a0889eac2e0
Benoit Parmentier <bparmentier@sshgw02.research.sesync.org>
2019-04-17 14:30:00 -0400

    moving functions to function script

--- harmonic_regression.R
+++ harmonic_regression.R
@@ -7,7 +7,7 @@
 ## DATE MODIFIED: 04/17/2019
 ## AUTHORS: Benoit Parmentier
 ## Version: 1
-## PROJECT: Time series analysis Managin Hurricanes
+## PROJECT: Time series analysis Managing Hurricanes
 ## ISSUE: 
 ## TO DO:
 ##
@@ -29,46 +29,11 @@ create_dir_fun <- function(outDir,out_suffix=NULL){
   return(outDir)
 }
 
-fit_harmonic <- function(p,n,y,mod_obj=F,figure=F){
-  t <- 1:n
-  omega=2*pi*p/n
-  cos_val =cos(omega*t)
-  sin_val =sin(omega*t)
-  
-  #plot(cos_val)
-  #plot(sin_val)
-  
-  in_df <- data.frame(y=y,cos_val=cos_val,sin_val=sin_val)
-  mod <- lm(y~ sin_val+ cos_val ,data=in_df)
-  summary(mod)
-  a <- mod$coefficients[2] #sine term
-  b <- mod$coefficients[3] #cosine term
-  A0 <- mod$coefficients[3] #mean
-  
-  A = sqrt(a^2 + b^2)
-  phase = atan(-b/a)
-  
-  ## Add p values later?
-  
-  harmonic_df <- data.frame(A0=A0,A=A,a=a,b=b,phase=phase,harmonic=p,omega=omega)
-  
-  ### Figure
-  if(figure==TRUE){
-    y_range <- range(mod$fitted.values,y,na.rm = T)
-    plot(mod$fitted.values,ylim=y_range)
-    points(y,col="blue",pch="+")
-  }
-  ###
-  harmonic_obj <- harmonic_df
-  
-  if(mod_obj==T){
-    harmonic_obj <- list(harmonic_df=harmonic_df,mod=mod)
-  }else{
-    harmonic_obj <- list(harmonic_df=harmonic_df)
-  }
-  ### 
-  return(harmonic_obj)
-}
+#Benoit setup
+script_path <- "/nfs/bparmentier-data/Data/projects/managing_hurricanes/scripts"
+
+crop_data_processing_functions <- "harmonic_regression_functions_04172019.R"
+source(file.path(script_path,crop_data_processing_functions))
 
 ############################################################################
 #####  Parameters and argument set up ###########
@@ -137,76 +102,8 @@ y <- y_all[1:24]
 n <- length(y)
 
 
-harmonic_regression<- function(y,n,harmonic_val=NULL,mod_obj=F,figure=F){
-  ##
-  # if mod_obj is True then return the model object 
-  #
-  ## Default to two first harmonic:
-  if(is.null(harmonic_val)){
-    p <- 1:2
-  }
-  #harmonic_val = 1 # pr from 1 to n/2
-  
-  #n<-24
-  
-  #omega_val = lapply(p,function(p){2*pi*p/n})
-  
-  l_harmonic_obj <- lapply(p,
-         FUN=fit_harmonic,
-         n=n,
-         y=y,
-         mod_obj=mod_obj,
-         figure=figure)
-  
-  l_df <- lapply(l_harmonic_obj,function(x){x$harmonic_df})
-  harmonic_df <- do.call(rbind,l_df)
-  rownames(harmonic_df) <- NULL
-  
-  #View(harmonic_df)
-  harmonic_results_obj  <- list(harmonic_df,l_harmonic_obj)
-  names(harmonic_results_obj) <- c("harmonic_df","l_harmonic_obj")
-  
-  return(harmonic_results_obj)
-}
+harmonic_results <- harmonic_regression(y,n,harmonic_val=NULL,mod_obj=F,figure=F)
 
-#first harmonic
-
-p = 1 #from 1 to n/2
-
-n<-24
-omega= 2*pi*p/n
-
-t <- 1:n
-cos_val =cos(omega*t)
-sin_val =sin(omega*t)
-plot(cos_val)
-plot(sin_val)
-
-in_df <- data.frame(y=y,cos_val=cos_val,sin_val=sin_val)
-mod <- lm(y~cos_val + sin_val,data=in_df)
-summary(mod)
-
-plot(y)
-lines(mod$fitted.values)
-points(mod$fitted.values)
-
-plot(mod$fitted.values,ylim=c(3000,10000))
-points(y,pch=2)
-
-plot(mod$fitted.values,ylim=c(4000,8000))
-points(y,pch=2)
-
-mod$coefficients[2]
-plot(y)
-
-#amplitude
-A = atan(-mod$coefficients[3]/mod$coefficients[2])
-
-A
-
-### FFT, does not work with missing values
-barplot(Im(fft(y)))
-plot(Re(fft(y)))
 
 ####################
 #### This is synthetic value
